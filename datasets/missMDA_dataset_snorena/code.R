05eb7d0cb6be3c0d2ba2e15f209b820cbd5e6f24
Elzbieta Jowik <elzbietajowik@gmail.com>
2020-04-21 21:10:14 +0200

    dataset upload

--- datasets/missMDA_dataset_snorena/code.R
+++ datasets/missMDA_dataset_snorena/code.R
@@ -13,8 +13,9 @@ id <- 1L
 data_name <- 'snorena'
 
 data(snorena)
+dataset <- as.data.frame(snorena)
 target_column <- 'snore'
 
 ## create json
-file <- CreateSummary(data = snorena, target_column = target_column, id = id, data_name = data_name, source = source, added_by = 'ejowik')
+file <- CreateSummary(data = dataset, target_column = target_column, id = id, data_name = data_name, source = source, added_by = 'ejowik')
 write(file, 'dataset.json')
